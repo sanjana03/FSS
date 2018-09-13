72c9071ceb7690674bc32e275a8e373fd04aeca8
sanjana03 <sanjana0309@gmail.com>
2018-09-13 00:25:31 -0400

    Code and output files for HW3
    Homework 3 - Code and Output files

---
+++ w3/sym.py
@@ -0,0 +1,38 @@
+import math
+class Sym:
+    def __init__(self, l, f=lambda x: x):
+        self.counts = dict()
+        self.mode = None
+        self.most = 0
+        self.n = 0
+        self.ent = None
+        for i in l:
+            self.symInc(f(i))
+
+    def symInc(self, x):
+        if x == "?":
+            return x
+        self.ent = None
+        self.n += 1
+        old = self.counts.get(x)
+        new = old + 1 if old else 1
+        self.counts[x] = new
+        if new > self.most:
+            self.most, self.mode = new, x
+        return x
+
+    def symDec(self, x):
+        self.ent = None
+        if self.n > 0:
+            self.n -= 1
+            self.counts[x] -= 1
+        return x
+
+    def symEnt(self):
+        if not self.ent:
+            self.ent = 0
+            p=0
+            for i in self.counts.values():
+                p = i / self.n
+                self.ent = self.ent - p*math.log(p,2)
+        return self.ent
---
+++ w3/w3.py
@@ -0,0 +1,61 @@
+import re, traceback
+import math
+from num import Num
+from sample import Sample
+from sym import Sym
+from random import random
+from random import seed
+
+
+class O:
+    y = n = 0
+
+    @staticmethod
+    def report():
+        print("\n# pass= %s fail= %s %%pass = %s%%" % (
+            O.y, O.n, int(round(O.y * 100 / (O.y + O.n + 0.001)))))
+
+    @staticmethod
+    def k(f):
+        try:
+            print("\n-----| %s |-----------------------" % f.__name__)
+            if f.__doc__:
+                print("# " + re.sub(r'\n[ \t]*', "\n# ", f.__doc__))
+            f()
+            print("# pass")
+            O.y += 1
+        except:
+            O.n += 1
+            print(traceback.format_exc())
+        return f
+
+@O.k
+def testSample():
+    seed(1)
+    s = []
+    for i in range(5, 11):
+        s.append(Sample(2 ** i))
+    for i in range(1, 10001):
+        y = random()
+        for t in s:
+            t.sampleInc(y)
+    for t in s:
+        print(t.maximum, t.nth(0.5))
+        assert 0.3 < t.nth(0.5) < 0.7
+
+
+@O.k
+def testNum():
+    n = Num([4, 10, 15, 38, 54, 57, 62, 83, 100, 100, 174,190, 215, 225, 233, 250, 260, 270, 299, 300, 306, 333, 350, 375, 443, 475, 525, 583, 780, 1000])
+    print(n.mu, n.sd)
+    assert n.mu == 270.3 and round(n.sd, 3) == 231.946
+
+
+@O.k
+def testSym():
+    sy = Sym(['y', 'y', 'y', 'y', 'y', 'y', 'y', 'y', 'y', 'n', 'n', 'n', 'n', 'n'])
+    print(sy.symEnt())
+    assert  round(sy.symEnt(),4) ==  0.9403
+
+if __name__ == "__main__":
+    O.report()
